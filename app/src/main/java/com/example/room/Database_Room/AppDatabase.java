d613e1b52e26002675d66bf614573ff37a44336d
parivendhan8 <parivendhan8@gmail.com>
2019-06-14 09:39:50 +0530

    veryfing latest commit

--- app/src/main/java/com/example/room/Database_Room/AppDatabase.java
+++ app/src/main/java/com/example/room/Database_Room/AppDatabase.java
@@ -6,6 +6,8 @@ import androidx.room.RoomDatabase;
 import com.example.room.Database_Room.Entities.Task;
 import com.example.room.Database_Room.Entities.TaskDao;
 
+
+
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
